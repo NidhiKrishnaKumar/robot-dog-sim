90a2902a5881da20c717767edd46ebe7323589f0
nkrishnakumar <nkrishnakumar@DESKTOP-DPEAVS4.>
2025-09-17 09:04:09 -0500

    actually added the python file this time

---
+++ visualize_dog.py
@@ -0,0 +1,13 @@
+import time
+from pathlib import Path
+import mujoco as mj
+from mujoco import viewer
+import numpy as np
+
+def walk_dog():
+
+
+
+if __name__ == "__main__": 
+    # Run indefinitely in real time; Ctrl+C or close window to exit.
+    walk_dog()
