a174973c5ce217ec416f32dede3f22d8ef0f7723
nkrishnakumar <nkrishnakumar@DESKTOP-DPEAVS4.>
2025-09-17 18:41:27 -0500

    finalized physical features of robot. TODO: update walking motion

--- visualize_dog.py
+++ visualize_dog.py
@@ -5,6 +5,24 @@ from mujoco import viewer
 import numpy as np
 
 def walk_dog():
+  """
+  Load a MuJoCo model (default floor.xml) and visualize it.
+  """
+  # Resolve path relative to this file's parent (project root assumption)
+  model = mj.MjModel.from_xml_path("xml/floor.xml")
+  data = mj.MjData(model)
+
+  dt = model.opt.timestep
+  start = time.time()
+  t = 0.0
+  viewer2 = viewer.launch_passive(model, data)
+  while True:
+    mj.mj_step(model, data)
+    print(data.qpos)
+    #data.ctrl = 10 * np.sin(t + np.array([0.0, 0.1, 0.2, 0.3]))
+    t += dt
+    time.sleep(dt)
+    viewer2.sync()
 
 
 
