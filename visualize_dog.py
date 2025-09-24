35db9be567d7a149caffbfc31618c14ec697ce52
nkrishnakumar <nkrishnakumar@DESKTOP-DPEAVS4.>
2025-09-24 18:38:17 -0500

    included some range of motion for dog still very buggy, falls over, non-smooth motion in lower leg

--- visualize_dog.py
+++ visualize_dog.py
@@ -4,6 +4,10 @@ import mujoco as mj
 from mujoco import viewer
 import numpy as np
 
+
+def gait(t, freq=1.0, phase=0.0):
+  return np.sin(2 * np.pi * freq * t + phase)
+  
 def walk_dog():
   """
   Load a MuJoCo model (default floor.xml) and visualize it.
@@ -15,11 +19,43 @@ def walk_dog():
   dt = model.opt.timestep
   start = time.time()
   t = 0.0
+  
+  # gait parameters
+  freq = 3
+  amp_elbow = 4
+  amp_arm = -0.7
+
   viewer2 = viewer.launch_passive(model, data)
   while True:
+    # Compute phase offsets for trot gait
+    fl_phase = 0.0
+    fr_phase = np.pi
+    bl_phase = np.pi
+    br_phase = 0.0
+
     mj.mj_step(model, data)
+
     print(data.qpos)
-    #data.ctrl = 10 * np.sin(t + np.array([0.0, 0.1, 0.2, 0.3]))
+    
+    # Compute joint commands
+    data.ctrl[:] = [
+      0.0,                                     # fl shoulder
+      amp_elbow * gait(t, freq, fl_phase),     # fl elbow
+      amp_arm * gait(t, freq, fl_phase),       # fl arm
+      
+      0.0,                                     # fr shoulder
+      amp_elbow * gait(t, freq, fr_phase),     # fr elbow
+      amp_arm * gait(t, freq, fr_phase),       # fr arm
+      
+      0.0,                                     # bl shoulder
+      amp_elbow * gait(t, freq, bl_phase),     # bl elbow
+      amp_arm * gait(t, freq, bl_phase),       # bl arm
+      
+      0.0,                                     # br shoulder
+      amp_elbow * gait(t, freq, br_phase),     # br elbow
+      amp_arm * gait(t, freq, br_phase),       # br arm
+    ]
+
     t += dt
     time.sleep(dt)
     viewer2.sync()
